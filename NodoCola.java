aa6e6340f38dce60edba3d6b910f76c5fab63696
UsainJordan <godinezazofeifajordan15@gmail.com>
2024-10-28 00:13:03 -0600

    Update NodoCola.java

--- NodoCola.java
+++ NodoCola.java
@@ -1,1 +1,31 @@
-package Jugadores;
+package jugadores;
+
+public class NodoCola {
+    private String partida;
+    private NodoCola atras;
+
+    public NodoCola(String partida) {
+        this.partida = partida;
+    }
+
+    public String getPartida() {
+        return partida;
+    }
+
+    public void setPartida(String partida) {
+        this.partida = partida;
+    }
+
+    public NodoCola getAtras() {
+        return atras;
+    }
+
+    public void setAtras(NodoCola atras) {
+        this.atras = atras;
+    }
+
+    @Override
+    public String toString() {
+        return "NodoCola{" + "partida=" + partida + '}';
+    }
+}
