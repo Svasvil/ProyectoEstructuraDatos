9b8efe991c65c8f24e0aaa587375cad81c45944f
UsainJordan <godinezazofeifajordan15@gmail.com>
2024-12-12 18:45:28 -0600

    Update NodosPila.java

--- ProyectoEdDGrupoA/src/proyectoeddgrupoa/NodosPila.java
+++ ProyectoEdDGrupoA/src/proyectoeddgrupoa/NodosPila.java
@@ -9,30 +9,37 @@ package proyectoeddgrupoa;
  * @author Andy
  */
 public class NodosPila {
-     private DatosJugador jugador; //Creamos los atributos
-    private NodosPila siguiente; //Creamos los atributos  
+    private DatosJugador jugador;  // Información del jugador
+    private NodosPila siguiente;   // Nodo siguiente
+
+    // Constructor vacío
     public NodosPila() {
-        this.siguiente = null; //Creamos un constructor vacío 
+        this.siguiente = null;
     }
-    public NodosPila(DatosJugador  jugador, NodosPila siguiente) { //Creamos un constructor 
+
+    // Constructor con parámetros
+    public NodosPila(DatosJugador jugador, NodosPila siguiente) {
         this.jugador = jugador;
-        this.siguiente = siguiente;
-    }
-    //Creamos los métodos getters y setters  
-    public DatosJugador  getJugador() { 
-        return jugador;
-    }
-    public void setJugador(DatosJugador  jugador) {
-        this.jugador = jugador;
-    }
-    public NodosPila getSiguiente() {
-        return siguiente;
-    }
-    public void setSiguiente(NodosPila siguiente) {
         this.siguiente = siguiente;
     }
 
+    // Getters y setters
+    public DatosJugador getJugador() {
+        return jugador;
     }
+
+    public void setJugador(DatosJugador jugador) {
+        this.jugador = jugador;
+    }
+
+    public NodosPila getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(NodosPila siguiente) {
+        this.siguiente = siguiente;
+    }
+}
     
 
 
