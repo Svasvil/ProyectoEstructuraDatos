a379ee789a49cfa327cde198bbd369812c28efc6
UsainJordan <godinezazofeifajordan15@gmail.com>
2024-12-12 18:49:44 -0600

    Update NodoLDE.java

--- ProyectoEdDGrupoA/src/proyectoeddgrupoa/NodoLDE.java
+++ ProyectoEdDGrupoA/src/proyectoeddgrupoa/NodoLDE.java
@@ -1,1 +1,44 @@
+public class NodoLDE {
+    private DatosJugador jugador;
+    private NodoLDE anterior;
+    private NodoLDE siguiente;
 
+    // Constructor vacío
+    public NodoLDE() {
+        this.jugador = null;
+        this.anterior = null;
+        this.siguiente = null;
+    }
+
+    // Constructor con parámetros
+    public NodoLDE(DatosJugador jugador) {
+        this.jugador = jugador;
+        this.anterior = null;
+        this.siguiente = null;
+    }
+
+    // Getters y setters
+    public DatosJugador getJugador() {
+        return jugador;
+    }
+
+    public void setJugador(DatosJugador jugador) {
+        this.jugador = jugador;
+    }
+
+    public NodoLDE getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(NodoLDE anterior) {
+        this.anterior = anterior;
+    }
+
+    public NodoLDE getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(NodoLDE siguiente) {
+        this.siguiente = siguiente;
+    }
+}
